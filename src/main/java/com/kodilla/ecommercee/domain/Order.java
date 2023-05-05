6027acc4715808093eebf97db4bfbb744fa900b6
Jakub <jakubfirlejczyk@gmail.com>
2023-05-05 23:58:24 +0200

    Order entity + OrderStatus

---
+++ src/main/java/com/kodilla/ecommercee/domain/Order.java
@@ -0,0 +1,20 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Table
+public class Order {
+    @Id
+    @GeneratedValue
+    private Long orderId;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+}
---
+++ src/main/java/com/kodilla/ecommercee/domain/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.kodilla.ecommercee.domain;
+
+public enum OrderStatus {
+    CREATED,
+    PENDING,
+    DONE
+
+
+}
