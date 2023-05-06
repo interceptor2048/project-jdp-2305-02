efa242d080c9dba76cb7a4c387f112677e874ed6
Bartosz Szych <bartoszych17@gmail.com>
2023-05-06 20:52:10 +0200

    JDP230502-3

---
+++ src/main/java/com/kodilla/ecommercee/controller/CartController.java
@@ -0,0 +1,32 @@
+package com.kodilla.ecommercee.controller;
+
+import com.kodilla.ecommercee.dto.CartDto;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cart")
+public class CartController {
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void cartCreation(@RequestBody CartDto CartDto){
+
+    }
+    @GetMapping(value = "{cartId}")
+    public CartDto getCartElements(@PathVariable Long cartId){
+        return null;
+
+    }
+    @PutMapping(value ="add")
+    public void addProductToCart(@RequestParam Long productId){
+
+    }
+    @PutMapping(value ="delete")
+    public void deleteProductFromCard(@RequestParam Long productId){
+
+    }
+    @PutMapping(value ="order")
+    public void makeOrderFromCart(){
+
+    }
+}
