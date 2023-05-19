5b0eb44a3c2e6e04c7d1ea9fabc1c654e211a1c9
Seweryn <sew_k@wp.pl>
2023-05-19 22:28:58 +0200

    JDP230502-21

--- src/main/java/com/kodilla/ecommercee/mapper/ProductMapper.java
+++ src/main/java/com/kodilla/ecommercee/mapper/ProductMapper.java
@@ -4,8 +4,10 @@ import com.kodilla.ecommercee.domain.Group;
 import com.kodilla.ecommercee.domain.Group;
 import com.kodilla.ecommercee.domain.Product;
 import com.kodilla.ecommercee.dto.ProductDto;
+import com.kodilla.ecommercee.exception.GroupNotFoundException;
 import com.kodilla.ecommercee.repository.GroupRepository;
 import com.kodilla.ecommercee.repository.GroupRepository;
+import com.kodilla.ecommercee.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,12 +17,16 @@ import java.util.stream.Collectors;
 @Service
 public class ProductMapper {
 
+    @Autowired
+    GroupRepository groupRepository;
+
     public Product mapToProduct(final ProductDto productDto) {
         return new Product(
                 productDto.getId(),
                 productDto.getName(),
                 productDto.getDescription(),
-                productDto.getPrice()
+                productDto.getPrice(),
+                groupRepository.findById(productDto.getGroupId()).orElseThrow(GroupNotFoundException::new)
         );
     }
     public ProductDto mapToProductDto(final Product product) {
@@ -28,7 +34,8 @@ public class ProductMapper {
                 product.getId(),
                 product.getName(),
                 product.getDescription(),
-                product.getPrice()
+                product.getPrice(),
+                product.getGroup().getId()
         );
     }
     public List<ProductDto> mapToProductDtoList(final List<Product> productList) {
